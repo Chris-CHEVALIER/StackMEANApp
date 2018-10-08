7d316189e0e0ece4ac35bf81bd58a6ab31348845
Mehdi <mehdiayache@hotmail.fr>
2018-10-08 16:45:56 +0200

    test

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 // Configuration du serveur NodeJS - Port : 3xxx
 const port = "3115";
 app.listen(port, function() {
-    console.log("CERIGame lancé sur le port", port);
+  console.log("CERIGame lancé sur le port", port);
 });
 
 // Gestion des URI
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/CERIGame/index.html"));
-    console.log(res);
+  res.sendFile(path.join(__dirname + "/CERIGame/index.html"));
+  console.log(res);
+  console.log('test');
 });
