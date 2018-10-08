0a589ad4360e5c3950a18aa642d1658a3a1e1ba0
Chevalier <chevalierchris34@gmail.com>
2018-10-08 16:36:37 +0200

    Configure server + Create a view (index.html)

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-/******** Chargement des Middleware
-*
-********/
+// Chargement des Middlewares :
+const express = require("express");
+const app = express();
+var path = require("path");
 
-/******** Declaration des variables
-*
-********/
+// Declaration des variables
+app.use("/css", express.static(__dirname + "/CERIGAME/css"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
-/******** Configuration du serveur NodeJS - Port : 3xxx
-*
-********/
+// Configuration du serveur NodeJS - Port : 3xxx
+const port = "3115";
+app.listen(port, function() {
+    console.log("CERIGame lancé sur le port", port);
+});
 
-/******** Gestion des URI
-*
-********/
-
+// Gestion des URI
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/CERIGame/index.html"));
+    console.log(res);
+});
